349be46425876849cf80da6613b6c2bda282c968
Harsh Anand <20harshanand24@gmail.com>
2026-09-03 16:10:45 +0530

    implemented data-driven layout mapping, using json array and loop

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,27 @@ import Human from './components/human'
 import StorageBin from './components/storageBin'
 import Conveyer from './components/conveyer'
 
+
+const initialLayout = [
+  { id: 'lathe_1', type: 'lathe', position: [1, 0.8, 1], rotation: [0, 0, 0] },
+  { id: 'drill_1', type: 'drill', position: [2, 0, -1], rotation: [0, 0, 0] },
+  { id: 'bench_1', type: 'workbench', position: [-3, 0, 1], rotation: [0, 0, 0] },
+  { id: 'rack_1', type: 'rack', position: [-14, 0, -2], rotation: [0, 0, 0] },
+  { id: 'human_1', type: 'human', position: [0, 0, 0], rotation: [0, 0, 0] },
+  { id: 'bin_1', type: 'bin', position: [0, 0, -1], rotation: [0, 0, 0] },
+  { id: 'conveyer_1', type: 'conveyer', position: [3, 0.8, 3], rotation: [0, 0, 0] }
+]
+
+const ComponentMap = {
+  lathe: Lathe,
+  drill: Drill,
+  workbench: Workbench,
+  rack: StorageRack,
+  human: Human,
+  bin: StorageBin,
+  conveyer: Conveyer
+}
+
 function App() {
   return (
     <Canvas style={{ position: 'fixed', inset: 0, width: '100vw', height: '100vh', backgroundColor: '#0f172a' }}>
@@ -22,14 +43,27 @@ function App() {
       <FactoryFloor width={25} length={20} />
 
 
-      <Lathe position={[1, 0.8, 1]} />
+      {/* <Lathe position={[1, 0.8, 1]} />
       <Drill position={[2, 0, -1]} />
       <Workbench position={[-3, 0, 1]} />
       <StorageRack position={[-14, 0, -2]} />
       <Human position={[0, 0, 0]} />
       <StorageBin position={[0, 0, -1]} />
-      <Conveyer position={[3, 0.8, 3]} />
+      <Conveyer position={[3, 0.8, 3]} /> */}
       
+      {/* dynamic rendering, using loop to render using the json array */}
+      {initialLayout.map((item) => {
+        const ModelComponent = ComponentMap[item.type];
+
+        return(
+          <ModelComponent
+             key = {item.id}
+             position = {item.position}
+             rotation = {item.rotation}
+          />
+        )
+
+      })}
 
       <OrbitControls 
         makeDefault 
