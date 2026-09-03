4a883b257544ba792c3743943e236328b0144b5a
Harsh Anand <20harshanand24@gmail.com>
2026-09-03 15:42:08 +0530

    added new model 'robot' as previous human model was rigid and updated human.jsx

--- src/components/human.jsx
+++ src/components/human.jsx
@@ -1,16 +1,17 @@
 import { useGLTF } from '@react-three/drei'
 
 export default function Human({ position = [0, 0, 0], rotation = [0, 0, 0] }) {
-    const { scene } = useGLTF('/models/human/human.glb')
+    const { scene } = useGLTF('/models/human/robot.glb')
 
     return (
         <group position={position} rotation={rotation}>
-            <primitive 
-            object={scene.clone()}
-            scale = {1}
-            />
+                <primitive 
+                object={scene.clone()}
+                scale = {1}
+                 />
+            
         </group>
     )
 }
 
-useGLTF.preload('/models/human/human.glb')
+useGLTF.preload('/models/human/robot.glb')
